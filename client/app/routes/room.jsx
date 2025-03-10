2e69d975508eb624547271c79f6546c0cd1dd0bc
Viipers <tsannie@student.42.fr>
2025-03-10 16:07:07 +0100

    fix rooms

--- client/app/routes/room.jsx
+++ client/app/routes/room.jsx
@@ -4,7 +4,7 @@ import { selectUsername } from '../redux/roomInfoSlice';
 
 const Room = () => {
   const username = useSelector((state) => selectUsername(state));
-  const rooms
+  //const rooms
 
   return (
     <div>
