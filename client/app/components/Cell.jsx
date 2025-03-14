3bb3932153a6775b3254faf3f51e47bde6a9cc41
Viipers <tsannie@student.42.fr>
2025-03-14 16:15:12 +0100

    merge

--- client/app/components/Cell.jsx
+++ client/app/components/Cell.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const Cell = ({ cell_value }) => {
-  return <div className={`w-10 h-10 border border-red-900 ${cell_value !== 0 ? 'bg-red-600' : ''}`} />;
+  const COLOR_CODE = {
+    c: 'bg-cyan-500',
+    b: 'bg-blue-500',
+    o: 'bg-orange-500',
+    g: 'bg-green-500',
+    p: 'bg-purple-500',
+    r: 'bg-red-500',
+    t: 'bg-black-500',
+  };
+
+  return <div className={`w-10 h-10 border border-red-900 ${COLOR_CODE[cell_value]}`} />;
 };
 
 export default Cell;
