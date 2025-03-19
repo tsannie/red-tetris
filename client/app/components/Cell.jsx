39e8f8d94b4b091a22d2bde54c801469fa26b50f
tsannie <theo@sannie.fr>
2025-03-19 20:46:16 +0100

    shadow cell

--- client/app/components/Cell.jsx
+++ client/app/components/Cell.jsx
@@ -10,10 +10,21 @@ const Cell = ({ cell_value }) => {
     p: 'bg-purple-500',
     r: 'bg-red-500',
     t: 'bg-black',
+    sc: 'border-1 border-cyan-500',
+    sb: 'border-1 border-blue-500',
+    sy: 'border-1 border-yellow-500',
+    so: 'border-1 border-orange-500',
+    sg: 'border-1 border-green-500',
+    sp: 'border-1 border-purple-500',
+    sr: 'border-1 border-red-500',
   };
 
   return (
-    <div className={`w-full h-full ${cell_value === 1 ? '' : 'border-1 border-black'} ${COLOR_CODE[cell_value]}`} />
+    <div
+      className={`w-full h-full ${cell_value === 1 || cell_value[0] === 's' ? '' : 'border-1 border-black'} ${
+        COLOR_CODE[cell_value]
+      }`}
+    />
   );
 };
 
